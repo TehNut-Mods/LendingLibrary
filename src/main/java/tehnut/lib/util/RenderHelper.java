478a63e353cf850784df73c7c5f502ed88ae2bed
Nicholas Ignoffo <speedynutty68@gmail.com>
2016-05-31 17:40:42 -0700

    Fix render layer adder in obf environments

--- src/main/java/tehnut/lib/util/RenderHelper.java
+++ src/main/java/tehnut/lib/util/RenderHelper.java
@@ -4,11 +4,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import tehnut.lib.LendingLibrary;
-
-import java.lang.reflect.Field;
 
 @SideOnly(Side.CLIENT)
 public class RenderHelper {
@@ -16,15 +15,10 @@ public class RenderHelper {
     public static boolean addPlayerLayerRenderer(Class<? extends LayerRenderer<?>> layerRendererClass) {
         RenderManager renderManager = Minecraft.getMinecraft().getRenderManager();
         try {
-            Field renderPlayerField = RenderManager.class.getDeclaredField("playerRenderer");
-            renderPlayerField.setAccessible(true);
-            Object renderPlayerObj = renderPlayerField.get(renderManager);
-            if (renderPlayerObj instanceof RenderPlayer) {
-                RenderPlayer renderPlayer = (RenderPlayer) renderPlayerObj;
-                LayerRenderer<?> layerRenderer = layerRendererClass.getConstructor(RenderPlayer.class).newInstance(renderPlayer);
-                renderPlayer.addLayer(layerRenderer);
-                return true;
-            }
+            RenderPlayer renderPlayer = ObfuscationReflectionHelper.getPrivateValue(RenderManager.class, renderManager, "playerRenderer", "field_178637_m");
+            LayerRenderer<?> layerRenderer = layerRendererClass.getConstructor(RenderPlayer.class).newInstance(renderPlayer);
+            renderPlayer.addLayer(layerRenderer);
+            return true;
         } catch (Exception e) {
             LendingLibrary.getLogger().error(e.getLocalizedMessage());
         }
