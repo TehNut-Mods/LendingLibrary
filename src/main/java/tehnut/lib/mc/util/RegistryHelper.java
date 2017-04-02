a8dcd521eef876b3df4e133fdd21389d25d4b4f7
Nicholas Ignoffo <speedynutty68@gmail.com>
2017-04-01 22:40:43 -0700

    Rewrite special-casing for RegistryHelper
    Now easier to special case things

--- src/main/java/tehnut/lib/mc/util/RegistryHelper.java
+++ src/main/java/tehnut/lib/mc/util/RegistryHelper.java
@@ -1,5 +1,7 @@
 package tehnut.lib.mc.util;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Maps;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
@@ -10,11 +12,49 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.common.registry.IForgeRegistryEntry;
 import net.minecraftforge.fml.common.registry.RegistryBuilder;
 import net.minecraftforge.fml.relauncher.Side;
+import org.apache.commons.lang3.tuple.Pair;
 import tehnut.lib.mc.block.BlockEnum;
 import tehnut.lib.mc.block.item.ItemBlockEnum;
 import tehnut.lib.mc.model.ModelHandler;
 
+import javax.annotation.Nullable;
+import java.util.Map;
+
 public class RegistryHelper {
+
+    private static final Map<Class<? extends IForgeRegistryEntry>, Function<Pair<IForgeRegistryEntry, String>, Void>> REGISTRY_CASES = Maps.newHashMap();
+
+    static {
+        REGISTRY_CASES.put(Block.class, new Function<Pair<IForgeRegistryEntry, String>, Void>() {
+            @SuppressWarnings({"MethodCallSideOnly", "unchecked"})
+            @Nullable
+            @Override
+            public Void apply(@Nullable Pair<IForgeRegistryEntry, String> input) {
+                if (input == null)
+                    return null;
+
+                Block block = (Block) input.getLeft();
+                GameRegistry.register(block.setRegistryName(input.getRight()));
+                if (block instanceof BlockEnum)
+                    GameRegistry.register(new ItemBlockEnum((BlockEnum) block).setRegistryName(input.getRight()));
+                if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT)
+                    ModelHandler.Client.handleBlockModel(block);
+                return null;
+            }
+        });
+        REGISTRY_CASES.put(Item.class, new Function<Pair<IForgeRegistryEntry, String>, Void>() {
+            @SuppressWarnings({"MethodCallSideOnly", "unchecked"})
+            @Nullable
+            @Override
+            public Void apply(@Nullable Pair<IForgeRegistryEntry, String> input) {
+                Item item = (Item) input.getLeft();
+                GameRegistry.register(item.setRegistryName(input.getRight()));
+                if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT)
+                    ModelHandler.Client.handleItemModel(item);
+                return null;
+            }
+        });
+    }
 
     public static <T extends IForgeRegistryEntry<T>> FMLControlledNamespacedRegistry<T> newRegistry(Class<T> type, String id, int min, int max) {
         RegistryBuilder<T> registryBuilder = new RegistryBuilder<T>();
@@ -24,19 +64,19 @@ public class RegistryHelper {
         return (FMLControlledNamespacedRegistry<T>) registryBuilder.create();
     }
 
-    @SuppressWarnings({"MethodCallSideOnly", "unchecked"})
-    public static void register(Block block, String name) {
-        GameRegistry.register(block.setRegistryName(name));
-        if (block instanceof BlockEnum)
-            GameRegistry.register(new ItemBlockEnum((BlockEnum) block).setRegistryName(name));
-        if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT)
-            ModelHandler.Client.handleBlockModel(block);
-    }
+    public static <T extends IForgeRegistryEntry<T>> T register(T type, String name) {
+        boolean reg = false;
+        for (Map.Entry<Class<? extends IForgeRegistryEntry>, Function<Pair<IForgeRegistryEntry, String>, Void>> entry : REGISTRY_CASES.entrySet()) {
+            if (entry.getKey().isAssignableFrom(type.getClass())) {
+                entry.getValue().apply(Pair.of((IForgeRegistryEntry) type, name));
+                reg = true;
+                break;
+            }
+        }
 
-    @SuppressWarnings({"MethodCallSideOnly"})
-    public static void register(Item item, String name) {
-        GameRegistry.register(item.setRegistryName(name));
-        if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT)
-            ModelHandler.Client.handleItemModel(item);
+        if (!reg)
+            GameRegistry.register(type.setRegistryName(new ResourceLocation(Loader.instance().activeModContainer().getModId(), name)));
+
+        return type;
     }
 }
